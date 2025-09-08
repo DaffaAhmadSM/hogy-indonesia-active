cd02f1b46b66ffa1fc77de0f366c96a0761d2b4a
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-09-08 08:44:43 +0700

    chore: change naming in login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,14 +13,14 @@
 <body>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Customer Sign In</h2>
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign In</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="/login" method="POST" class="space-y-6">
                 @csrf
                 <div>
-                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Username</label>
                     <div class="mt-2">
                         <input id="name" type="text" name="name"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
