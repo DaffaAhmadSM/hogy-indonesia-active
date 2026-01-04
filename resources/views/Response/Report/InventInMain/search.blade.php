e75b80dafc76c06e7796dade34be00855279904f
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2026-01-04 12:46:52 +0700

    fix: date formating

--- resources/views/Response/Report/InventInMain/search.blade.php
+++ resources/views/Response/Report/InventInMain/search.blade.php
@@ -1,10 +1,15 @@
+@php
+    use Illuminate\Support\Carbon;
+@endphp
 @foreach ($prod_receipt->items() as $item)
     <tr class="*:text-gray-900 *:first:font-medium">
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->BC_CODE_NAME }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NOMORDAFTAR }}</td>
-        <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->TANGGALDAFTAR }}</td>
+        <td class="px-3 py-2 whitespace-normal break-words align-top">
+            {{ Carbon::parse($item->TANGGALDAFTAR)->format('Y-m-d') }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NOMORPENERIMAAN }}</td>
-        <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->TANGGALPENERIMAAN }}</td>
+        <td class="px-3 py-2 whitespace-normal break-words align-top">
+            {{ Carbon::parse($item->TANGGALPENERIMAAN)->format('Y-m-d') }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->PENGIRIM }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->KODEBARANG }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NAMABARANG }}</td>
--- resources/views/Response/Report/InventOutMain/search.blade.php
+++ resources/views/Response/Report/InventOutMain/search.blade.php
@@ -1,16 +1,22 @@
+@php
+    use Illuminate\Support\Carbon;
+@endphp
+
 @foreach ($prod_receipt->items() as $item)
     <tr class="*:text-gray-900 *:first:font-medium"
         hx-get="{{ route('report.invent-out-main.detail', ['state' => 'archived', 'salesPickLineRecId' => $item->RECID]) }}"
         hx-target="#modal-table" hx-swap="innerHTML" hx-trigger="click">
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->BC_CODE_NAME }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NOMORDAFTAR }}</td>
-        <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->TANGGALDAFTAR }}</td>
+        <td class="px-3 py-2 whitespace-normal break-words align-top">
+            {{ Carbon::parse($item->TANGGALDAFTAR)->format('Y-m-d') }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NOMORPENGIRIMAN }}</td>
-        <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->TANGGALPENGIRIMAN }}</td>
+        <td class="px-3 py-2 whitespace-normal break-words align-top">
+            {{ Carbon::parse($item->TANGGALPENGIRIMAN)->format('Y-m-d') }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->PENERIMA }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->KODEBARANG }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->NAMABARANG }}</td>
-         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ number_format($item->JUMLAH, 2, ',', '.') }}
+        <td class="px-3 py-2 whitespace-normal break-words align-top">{{ number_format($item->JUMLAH, 2, ',', '.') }}
         </td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ $item->SATUAN }}</td>
         <td class="px-3 py-2 whitespace-normal break-words align-top">{{ number_format($item->NILAI, 2, ',', '.') }}
