01423e66fdf399ba7fe7e88baf81997f1b84fab9
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2026-01-11 09:16:39 +0700

    refactor: move pemasukan pengeluaran to db active

--- app/Models/ReportPemasukan.php
+++ app/Models/ReportPemasukan.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReportPemasukan extends Model
 {
-    protected $table = 'NSI_7REPORTSBCPEMASUKANARCHIVE';
+    protected $table = 'NSI_7REPORTSBCPEMASUKAN_V';
     protected $primaryKey = 'RECID'; // Assuming there's a primary key named 'ID'
 }
--- app/Models/ReportPengeluaran.php
+++ app/Models/ReportPengeluaran.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReportPengeluaran extends Model
 {
-    protected $table = 'NSI_7REPORTSBCPENGELUARANARCHIVE';
+    protected $table = 'NSI_7REPORTSBCPENGELUARAN_V';
     protected $primaryKey = 'RECID'; // Assuming there's a primary key named 'ID'
 }
